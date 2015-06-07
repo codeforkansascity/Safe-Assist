07abc8078c9daf3a19c8d63f23a524c3228e8c3a
jmorwick <jbmorwick@gmail.com>
2015-06-07 10:49:28 -0500

    added keys and other modifications
    added primary keys, removed NOT NULL constraint from a field that is
    optional

--- model/database_schema.sql
+++ model/database_schema.sql
@@ -1,87 +1,90 @@
--- MySQL dump 10.13  Distrib 5.6.22, for osx10.8 (x86_64)
---
--- Host: localhost    Database: safeassist
--- ------------------------------------------------------
--- Server version	5.6.25
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `AGENT`
---
-
-DROP TABLE IF EXISTS `AGENT`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `AGENT` (
-  `ID` int(11) NOT NULL,
-  `USER_ID` int(11) NOT NULL,
-  `Dept` varchar(80) NOT NULL,
-  `Policyname` varchar(80) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `CONSUMER`
---
-
-DROP TABLE IF EXISTS `CONSUMER`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `CONSUMER` (
-  `ID` int(11) NOT NULL,
-  `User_id` int(11) NOT NULL,
-  `Address` varchar(100) NOT NULL,
-  `Safety` varchar(100) NOT NULL,
-  `Medical_issue` varchar(225) NOT NULL,
-  `Medical_require` varchar(225) NOT NULL,
-  `Freetest` text NOT NULL,
-  `Contact` varchar(225) NOT NULL,
-  `Update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Interaction` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `USER`
---
-
-DROP TABLE IF EXISTS `USER`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `USER` (
-  `ID` int(11) NOT NULL,
-  `Oauth` varchar(40) NOT NULL,
-  `Username` varchar(85) NOT NULL,
-  `Password` varchar(85) NOT NULL,
-  `Usertype` enum('AGENT','ADMINISTRATOR','NORMAL') DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping events for database 'safeassist'
---
-
---
--- Dumping routines for database 'safeassist'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-06-07 10:21:35
+	-- MySQL dump 10.13  Distrib 5.6.22, for osx10.8 (x86_64)
+	--
+	-- Host: localhost    Database: safeassist
+	-- ------------------------------------------------------
+	-- Server version	5.6.25
+	
+	/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+	/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+	/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+	/*!40101 SET NAMES utf8 */;
+	/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+	/*!40103 SET TIME_ZONE='+00:00' */;
+	/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+	/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+	/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+	/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+	
+	--
+	-- Table structure for table `AGENT`
+	--
+	
+	DROP TABLE IF EXISTS `AGENT`;
+	/*!40101 SET @saved_cs_client     = @@character_set_client */;
+	/*!40101 SET character_set_client = utf8 */;
+	CREATE TABLE `AGENT` (
+	  `ID` int(11) NOT NULL AUTO_INCREMENT,
+	  `USER_ID` int(11) NOT NULL,
+	  `Dept` varchar(80) NOT NULL,
+	  `Policyname` varchar(80) NOT NULL,
+	  PRIMARY KEY (`ID`)
+	) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+	/*!40101 SET character_set_client = @saved_cs_client */;
+	
+	--
+	-- Table structure for table `CONSUMER`
+	--
+	
+	DROP TABLE IF EXISTS `CONSUMER`;
+	/*!40101 SET @saved_cs_client     = @@character_set_client */;
+	/*!40101 SET character_set_client = utf8 */;
+	CREATE TABLE `CONSUMER` (
+	  `ID` int(11) NOT NULL AUTO_INCREMENT,
+	  `User_id` int(11) NOT NULL,
+	  `Address` varchar(100) NOT NULL,
+	  `Safety` varchar(100) NOT NULL,
+	  `Medical_issue` varchar(225) NOT NULL,
+	  `Medical_require` varchar(225) NOT NULL,
+	  `Freetest` text NOT NULL,
+	  `Contact` varchar(225) NOT NULL,
+	  `Update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	  `Interaction` varchar(100) NOT NULL,
+	  PRIMARY KEY (`ID`)
+	) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+	/*!40101 SET character_set_client = @saved_cs_client */;
+	
+	--
+	-- Table structure for table `USER`
+	--
+	
+	DROP TABLE IF EXISTS `USER`;
+	/*!40101 SET @saved_cs_client     = @@character_set_client */;
+	/*!40101 SET character_set_client = utf8 */;
+	CREATE TABLE `USER` (
+	  `ID` int(11) NOT NULL AUTO_INCREMENT,
+	  `Oauth` varchar(40),
+	  `Username` varchar(85) NOT NULL,
+	  `Password` varchar(85) NOT NULL,
+	  `Usertype` enum('AGENT','ADMINISTRATOR','NORMAL') DEFAULT NULL,
+	  PRIMARY KEY (`ID`)
+	) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+	/*!40101 SET character_set_client = @saved_cs_client */;
+	
+	--
+	-- Dumping events for database 'safeassist'
+	--
+	
+	--
+	-- Dumping routines for database 'safeassist'
+	--
+	/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+	
+	/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+	/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+	/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+	/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+	/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+	/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+	
+	-- Dump completed on 2015-06-07 10:21:35
