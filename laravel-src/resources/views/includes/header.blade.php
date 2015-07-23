8451e5f27e427ee0e64303e1ab5cb78747f0ce98
jmorwick <jbmorwick@gmail.com>
2015-07-22 23:57:55 -0500

    moved some page content out in to other blades

---
+++ laravel-src/resources/views/includes/header.blade.php
@@ -0,0 +1,16 @@
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<a href="index.html" target="_top"><img src="images/safe-assist-banner.jpg" title="Safe Assist Logo" alt="Safe Assist Logo" /></a>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="left-sidebar.html">About</a></li>
+						<li><a href="right-sidebar.html">Contact</a></li>
+						<li><a href="no-sidebar.html">Partners</a></li>
+						<li><a href="auth/login" class="button special">Sign In</a></li>
+					</ul>
+				</nav>
+			</header>
+			
+			@include('auth.login')
