cc1f8a896df142f72cb0a474ddf7d144386abced
jmorwick <jbmorwick@gmail.com>
2015-11-22 23:25:17 -0600

    test

--- laravel-src/resources/views/includes/header.blade.php
+++ laravel-src/resources/views/includes/header.blade.php
@@ -20,7 +20,7 @@
 				<a href="/" target="_top"><img src="images/safe-assist-banner.jpg" title="Safe Assist Logo" alt="Safe Assist Logo" /></a>
 				<nav id="nav">
 					<ul>
-						<li><a href="/">Home</a></li>
+						<li><a href="/">Home</a> </li>
 						<li><a href="/about">About</a></li>
 						<li><a href="/contact">Contact</a></li>
 						<li><a href="/partners">Partners</a></li>
