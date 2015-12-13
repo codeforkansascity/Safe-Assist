f0ff8f8780867dca0e23afdd8f1472082e58d159
jmorwick <jbmorwick@gmail.com>
2015-12-12 23:48:17 -0600

    organized ui templates

--- laravel-src/app/Http/routes.php
+++ laravel-src/app/Http/routes.php
@@ -1,32 +1,30 @@
 <?php
 
-Route::get('/', ['as' => 'welcome', function () {
-    return view('welcome');
-}]);
-Route::get('/auth/loginform', ['as' => 'welcome', function () {
-    return view('auth.login');
-}]);
-
+// public info routes
 Route::get('/home', function() { return Redirect::to('/'); });
-
-
 Route::get('/about',  function () { return view('about'); });
 Route::get('/contact',  function () { return view('contact'); });
 Route::get('/partners',  function () { return view('partners'); });
+Route::get('/', ['as' => 'welcome', function () {
+    return view('welcome');
+}]);
 
-
+// UI routes
 Route::get('/profile',  function () { return view('user_profile'); });
 Route::get('/admin',  function () { return view('administrator_ui'); });
 Route::get('/caregiver',  function () { return view('caregiver_ui'); });
 Route::get('/agent',  function () { return view('agent_ui'); });
 
+// registration routes
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
-// Authentication routes...
+
+// authentication routes
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('/auth/loginform', ['as' => 'welcome', function () {
+    return view('auth.login');
+}]);
 
