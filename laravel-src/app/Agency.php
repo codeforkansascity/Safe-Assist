db541625007685d2ebc7f5712c748657ad396f7f
jmorwick <jbmorwick@gmail.com>
2015-12-13 13:43:54 -0600

    linked agencies back to their users

--- laravel-src/app/Agency.php
+++ laravel-src/app/Agency.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Agency extends Model
 {
 	protected $table = 'agencies';
+	
+	public function agents() {
+		return $this->belongsToMany('App\User', 'users_has_agencies');
+    	}
 }
