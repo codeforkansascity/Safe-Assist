07e044eb6a91b7697781ab3ee4c01cb5e8ce78c5
jmorwick <jbmorwick@gmail.com>
2015-12-31 09:10:16 -0600

    comment fix

--- laravel-src/app/Http/Controllers/UI/UserController.php
+++ laravel-src/app/Http/Controllers/UI/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
     }
     
     /**
-     * Reset the given user's password.
+     * update the given user's profile info.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
