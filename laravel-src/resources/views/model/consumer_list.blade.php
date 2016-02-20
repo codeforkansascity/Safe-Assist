a2449285d68edbebd7188b6f96565528e2fd57a8
jmorwick <jbmorwick@gmail.com>
2016-02-20 00:38:40 -0600

    fix path missed from previous refactor

--- laravel-src/resources/views/model/consumer_list.blade.php
+++ laravel-src/resources/views/model/consumer_list.blade.php
@@ -2,7 +2,7 @@
     @foreach ($consumers as $consumer)
         @include('model.consumer', ['consumer' => $consumer])
         @if(Auth::user()->consumers->contains('id', $consumer->id))
-            <a class="button" href="consumer_edit/{{$consumer->id}}">edit</a>
+            <a class="button" href="/consumer/edit/{{$consumer->id}}">edit</a>
         @endif
     @endforeach
 </div>
