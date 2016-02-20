3138773c248180d77fcffca1ec6a80be438c055c
jmorwick <jbmorwick@gmail.com>
2016-02-20 12:06:28 -0600

    updated user search templates

---
+++ laravel-src/resources/views/model/user_list.blade.php
@@ -0,0 +1,6 @@
+<div class="user_list">
+    @foreach ($users as $user)
+        @include('model.user', ['uset' => $user])
+        <a class="button" href="user_edit/{{$user->id}}">edit</a>
+    @endforeach
+</div>
