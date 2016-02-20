4d63f859097ade05c648b433ec3d5ece9e366510
jmorwick <jbmorwick@gmail.com>
2016-02-19 21:09:35 -0600

    simple user search by id

--- laravel-src/resources/views/ui/agent_ui.blade.php
+++ laravel-src/resources/views/ui/agent_ui.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-TODO: add content
+<select name="agency">
+    <h1>Search For Consumers</h1>
+    <form action="">
+        {!! csrf_field() !!}
+        @foreach(Auth::user()->agencies as $agency)
+        <option value="$agency->id">{{$agency->name}}</option>
+        @endforeach
+        <div class="form_row">
+            <div class="label">User ID</div>
+            <input type="text" name="keyword"/>
+        </div>
+        <input type="submit" value="search" />
+    </form>
+</select>
 @stop
