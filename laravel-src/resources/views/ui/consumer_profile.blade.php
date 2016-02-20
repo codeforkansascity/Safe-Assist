f745f780094c9e69da76b57708074d3d380e646b
jmorwick <jbmorwick@gmail.com>
2016-02-20 00:38:26 -0600

    provide a page for viewing a single consumer

--- laravel-src/resources/views/ui/consumer_profile.blade.php
+++ laravel-src/resources/views/ui/consumer_profile.blade.php
@@ -1,5 +1,16 @@
-<section class="consumer_profile">
-	<div class="first_name">{{$user->first_name}}</div>
-	<div class="last_name">{{$user->last_name}}</div>
-	@include('model.address', ['address' => $user->address])
-</section>
+@extends('layouts.master')
+
+@section('content')
+	@include('model.consumer', ['consumer' => $consumer])
+	@if (Auth::user()->administrator || Auth::user()->consumers->contains($consumer))
+		<a href="/consumer/edit/{{$consumer->id}}" class="button special">Edit Consumer</a>
+	@endif
+
+	@if (Auth::user()->consumers->contains($consumer))
+		<form action="/consumer/delete" method="POST">
+			{!! csrf_field() !!}
+			<input type="hidden" name="id" value="{{$consumer->id}}"/>
+			<input type="submit" value="Delete Consumer" class="button special">
+		</form>
+	@endif
+@stop
