9eb9034ae2eca79ff77318d9157f2e4351f89168
Joey Kendall-Morwick <jbmorwick@gmail.com>
2016-05-23 19:13:38 -0500

    updated dropdown to allow multiselect

--- laravel-src/resources/views/model/form/dropdown.blade.php
+++ laravel-src/resources/views/model/form/dropdown.blade.php
@@ -1,8 +1,12 @@
 <div class="form_row">
 	<div class="label">{{$description}}</div> 
-	<select name="{{$name}}">
+	<select name="{{$name}}" @if(isset($values)) multiple @endif >
 	@foreach ($options as $option_name => $option_description )
-		<option value="{{$option_name}}" {{ (old($name) ? old($name) : $value) == $option_name ? 'SELECTED' : '' }}>{{$option_description}}</option>
+	    @if(isset($values))
+			<option value="{{$option_name}}" {{ in_array($option_name, $values) ? 'SELECTED' : '' }}>{{$option_description}}</option>
+		@else
+		    <option value="{{$option_name}}" {{ (old($name) ? old($name) : $value) == $option_name ? 'SELECTED' : '' }}>{{$option_description}}</option>
+		@endif
 	@endforeach
 	</select>
 </div>
