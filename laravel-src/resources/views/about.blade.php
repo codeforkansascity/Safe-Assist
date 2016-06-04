595d426da8fd11d2b1f7c1b83e97f54ff28a653c
Joey Kendall-Morwick <jbmorwick@gmail.com>
2016-06-04 15:51:51 -0500

    added about us content
    provided by Susan Law

--- laravel-src/resources/views/about.blade.php
+++ laravel-src/resources/views/about.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.master')
 
 @section('content')
-TODO: add content
+
+    <h2>Our Purpose</h2>
+    <p>The purpose of Safe Assist is to provide information about your at-risk dependent to safety personnel.  Dependents that are at-risk include children and adults who may have difficulty communicating and/other special needs due to Alzheimer’s disease, dementia, autism, down syndrome, developmental or cognitive disability, and mental disabilities. 
+    The Kansas City Police Department would appreciate your help in providing them with information that may help your loved one in time of need.
+    </p>
+
+    <h2>Frequently Asked Questions</h2>
+
+    <h3>Do I have to register with Safe Assist?</h3>
+    <p>No.  Registering your dependent is completely voluntary.</p>
+
+    <h3>Who can register a dependent?</h3>
+    <p>The following may register a dependent:
+    </p>
+    <ul>
+        <li>Individuals with special needs</li>
+        <li>Parents or guardians of minor children who have special needs</li>
+        <li>Those with legal guardianship for an individual with special needs</li>
+        <li>Current foster care parents of a child living in their home with special needs</li>
+        <li>A family member living at the same residence who has special needs.</li>
+    </ul>
+
+    <h2>Do I have to fill out the information all at one time?</h2>
+    <p>No.  You may fill out the form during multiple sittings.  The Safe Assist Program will save your progress so you may stop and continue at your convenience by logging back in to the program.</p>
+
+    <h2>What if I need to change the information I provided?</h2>
+    <p>You may change information at any time by signing in to the program.</p>
+
+    <h2>Who has access to the information I provide?</h2>
+    <p>When an emergency call is received with regards to a specific individual with special needs, the dispatcher accesses the information you provide in the Safe Assist Program.  The dispatcher may provide information to emergency personnel to assist them in their call for service.</p>
+
+
 @stop
