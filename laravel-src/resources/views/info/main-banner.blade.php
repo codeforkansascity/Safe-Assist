552ceb94d9079d5a9ff78935503168e93efd028d
Joey Kendall-Morwick <jbmorwick@gmail.com>
2016-06-05 12:33:32 -0500

    removed unused buttons from front page

--- laravel-src/resources/views/info/main-banner.blade.php
+++ laravel-src/resources/views/info/main-banner.blade.php
@@ -5,13 +5,6 @@
 		@if (Auth::check()) 
 			<p>Welcome, {!! Auth::user()->email !!}</p> 
 		@endif
-
-		@if (!Auth::check()) 
-			<a id="enroll_button" class="btn btn-primary btn-lg">Enroll</a>
-		@else 
-			<a href="caregiver" class="btn btn-danger btn-lg">Caregiver Menu</a>
-		@endif
-			<a href="#elements" class="btn btn-primary btn-lg">Learn More</a>
 	</div>
 </section>
 
